06e0c2d704491e028f1a4855ac58dc19d21dc77e
gwuix2 <coolreef@gmail.com>
2013-12-23 14:03:17 +0100

    beginning

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gerse::Application.config.secret_key_base = '967dcb662c6125567ddc18594292f5c3a9b1a430d52d648a49b5caf0bbcce96116dde9c80701472ef39ed8f013c943bd9ee59f2f7ddfdab5881a0c92f35e0696'
